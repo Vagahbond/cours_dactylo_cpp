49ee747fd14f6694a1f47c7409d88f2c7819a9f4
Yoni <firroloni.yoni@gmail.com>
2021-03-14 23:47:38 +0100

    add dictionnary

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <fstream>
+#include <experimental/filesystem>
+#include <string>
+#include <vector>
+
+namespace fs = std::experimental::filesystem;
 
 int main(int argc, char const *argv[])
 {
-  std::cout << "Hello world" << std::endl;
+  std::cout << "Welcome to this dactylo skills measuring software. Words will be given to you and you will have to type them the fastest you can !" << std::endl;
+
+  if (!fs::exists("dictionnary")) {
+    std::cerr << "Please provide a dictionnary !" << std::endl;
+    exit(1); 
+  }
+
+  std::ifstream stream("dictionnary", std::ios_base::in);
+  std::vector<std::string> dictionnary;
+
+  std::string temp;
+
+  while(std::getline(stream, temp)) {
+      dictionnary.push_back(temp);
+  }
+
+  for (int i = 0; i < dictionnary.size(); ++i) {
+    std::cout << dictionnary[i] << std::endl;
+  }
+
   return 0;
 }
